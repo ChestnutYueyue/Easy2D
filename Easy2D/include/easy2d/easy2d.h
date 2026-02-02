982ec46d0cac03f21c69ea005e89b95d0c1ecf20
ChestnutYueyue <952134128@qq.com>
2026-02-03 00:28:48 +0800

    refactor: 移除过时的版本检查和调试库链接
    删除对 Visual Studio 2012 以下版本的检查，因为不再需要支持这些旧版本
    移除调试模式的库链接配置，改为由构建系统处理

--- Easy2D/include/easy2d/easy2d.h
+++ Easy2D/include/easy2d/easy2d.h
@@ -12,10 +12,6 @@
 #	error 仅能在 C++ 环境下使用 Easy2D
 #endif
 
-#if _MSC_VER < 1700
-#	error Easy2D 不支持 Visual Studio 2012 以下版本
-#endif
-
 
 #include <easy2d/e2dmacros.h>
 #include <easy2d/e2dmath.h>
@@ -31,8 +27,4 @@
 #include <easy2d/e2daction.h>
 #include <easy2d/e2dtransition.h>
 
-#if defined(DEBUG) || defined(_DEBUG)
-#	pragma comment(lib, "libeasy2dd.lib")
-#else
-#	pragma comment(lib, "libeasy2d.lib")
-#endif
+
