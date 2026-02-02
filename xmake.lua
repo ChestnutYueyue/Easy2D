b104b726de5a11752d6f3939a446cdb94276ca90
ChestnutYueyue <952134128@qq.com>
2026-02-02 09:21:18 +0800

    build: 重构 xmake 配置并添加 easy2d 和 GreedyMonster 目标
    重构 xmake.lua 配置文件，清理注释并重新组织依赖项。添加 easy2d 静态库目标，包含所有源文件和头文件，并配置平台特定的编译选项。同时添加 GreedyMonster 可执行目标，依赖 easy2d 并设置运行目录和 Windows 平台特定的资源文件处理。

--- xmake.lua
+++ xmake.lua
@@ -1,24 +1,68 @@
--- Easy2D 项目 xmake 配置文件
-
--- 设置项目名称
 set_project("Easy2D")
-
--- 设置项目版本
 set_version("3.0.0")
-
--- 设置 C++ 标准
 set_languages("c++17")
-
--- 添加编译模式规则
 add_rules("mode.debug", "mode.release")
 
--- 添加依赖库
-add_requires("miniaudio", "glad", "freetype", "stb", "zlib", "glm", "simpleini")
--- 使用 SDL3 替代 SDL2
 add_requires("libsdl3")
+add_requires("glad")
+add_requires("stb")
+add_requires("zlib")
+add_requires("glm")
+add_requires("simpleini")
+add_requires("miniaudio")
 add_requires("freetype", { configs = { zlib = true } })
 add_requires("spdlog", { configs = { header_only = true } })
 
+target("easy2d")
+    set_kind("static")
+    set_basename("libeasy2d")
+    add_files("Easy2D/src/**.cpp")
+    add_headerfiles("Easy2D/include/**.h")
+    add_includedirs("Easy2D/include", {public = true})
+    add_packages(
+        "libsdl3",
+        "glad",
+        "stb",
+        "zlib",
+        "glm",
+        "simpleini",
+        "miniaudio",
+        "freetype",
+        "spdlog",
+        {public = true}
+    )
 
+    if is_plat("windows") then
+        add_defines("WIN32_LEAN_AND_MEAN")
+        add_syslinks("opengl32", "user32", "gdi32", "shell32", "winmm", "imm32", "version", "ole32")
 
---
+        local toolchain = get_config("toolchain") or "msvc"
+        if toolchain == "msvc" or toolchain == "clang-cl" then
+            add_cxxflags("/EHsc", "/Zc:__cplusplus", "/utf-8", {force = true})
+        else
+            add_cxxflags("-fexceptions", "-frtti", "-finput-charset=UTF-8", "-fexec-charset=UTF-8")
+        end
+
+        if toolchain == "msvc" or toolchain == "clang-cl" then
+            if is_mode("debug") then
+                set_runtimes("MDd")
+            else
+                set_runtimes("MD")
+            end
+        end
+    else 
+        add_cxxflags("-finput-charset=utf-8", "-fexec-charset=utf-8")
+    end
+
+target("GreedyMonster")
+    set_kind("binary")
+    add_deps("easy2d")
+    add_files("GreedyMonster-Easy2D/src/**.cpp")
+    add_includedirs("GreedyMonster-Easy2D/src")
+    set_rundir("GreedyMonster-Easy2D/src")
+
+    if is_plat("windows") then
+        add_cxxflags("/utf-8")
+        add_rules("win.sdk.resource")
+        add_files("GreedyMonster-Easy2D/src/*.rc")
+    end
