787e0196536c60dce87f7e7741b2955f8cfd19d8
ChestnutYueyue <952134128@qq.com>
2026-02-05 00:58:18 +0800

    fix: 回退版本号至2.1.28并解决合并冲突

--- xmake.lua
+++ xmake.lua
@@ -6,7 +6,7 @@
 
 -- 1. 项目基础配置（全局）
 set_project("Easy2D")
-set_version("2.1.29")
+set_version("2.1.28")
 set_languages("c++17")
 set_encodings("utf-8")
 add_rules("mode.debug", "mode.release")
@@ -103,10 +103,4 @@ target("easy2d")
         end
     end
 target_end()
-<<<<<<< HEAD
 
-=======
-        end
-    end)
-target_end()
->>>>>>> e43c8cf9bdfdfe10ad8cbcc44578020ab741c122
